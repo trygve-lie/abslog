f7c5e875ed2287b41420a5ad1554c7dde4b187f6
Trygve Lie <post@trygve-lie.com>
2017-11-25 01:29:18 +0100

    Adding tests

--- test/log.js
+++ test/log.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// const log = require('../lib/log');
+const utils = require('../lib/utils');
+const log = require('../lib/log');
 const tap = require('tap');
 
 // https://www.npmjs.com/package/loglevel
@@ -10,7 +11,62 @@ const tap = require('tap');
 // https://www.npmjs.com/package/winston
 // https://www.npmjs.com/package/debug
 
-tap.test('abst-log() - foo - should bar', (t) => {
-    t.equal('foo', 'foo');
+function noop() {}
+
+tap.test('log() - no logger set - should return noop logger', (t) => {
+    const l = log();
+    t.true(utils.validateMethod(l.fatal));
+    t.true(utils.validateMethod(l.error));
+    t.true(utils.validateMethod(l.warn));
+    t.true(utils.validateMethod(l.info));
+    t.true(utils.validateMethod(l.debug));
+    t.true(utils.validateMethod(l.trace));
+    t.end();
+});
+
+tap.test('log() - set compatible logger - should return set logger', (t) => {
+    const logger = {
+        fatal: noop,
+        error: noop,
+        warn: noop,
+        info: noop,
+        debug: noop,
+        trace: noop
+    };
+
+    const l = log(logger);
+    t.equals(l.fatal, logger.fatal);
+    t.equals(l.error, logger.error);
+    t.equals(l.warn, logger.warn);
+    t.equals(l.info, logger.info);
+    t.equals(l.debug, logger.debug);
+    t.equals(l.trace, logger.trace);
+    t.end();
+});
+
+tap.test('log() - console set - should return console', (t) => {
+    const l = log(console);
+    t.equals(l.fatal, console.log);
+    t.equals(l.error, console.error);
+    t.equals(l.warn, console.warn);
+    t.equals(l.info, console.info);
+    t.equals(l.debug, console.debug);
+    t.equals(l.trace, console.trace);
+    t.end();
+});
+
+tap.test('log() -  set non compatible logger - should throw', (t) => {
+    const logger = {
+        error: noop,
+        warn: noop,
+        info: noop,
+        debug: noop,
+        trace: noop
+    };
+
+    t.throws(() => {
+        log(logger);
+    }, new TypeError('Provided logger is not API compliant. Missing "fatal" method.'));
+
     t.end();
 });
