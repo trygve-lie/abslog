897a8ff8318862ebbe75a0cfd75c2d7622247d58
Trygve Lie <post@trygve-lie.com>
2018-10-07 19:27:43 +0200

    Test SyncLog

---
+++ test/logger.synclog.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const SyncLog = require('synclog');
+const tap = require('tap');
+const log = require('../lib/log');
+
+tap.test('synclog() - set synclog as logger - should return synclog', (t) => {
+    const synclog = new SyncLog('debug');
+    const logger = log(synclog);
+    t.equals(logger, synclog);
+    t.end();
+});
